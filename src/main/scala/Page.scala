bfde191615c7167360574c8a05c1856a4de8273d
Tom Liddle <tom.liddle@gmail.com>
2016-01-27 12:37:51 +0000

    Add page case class

---
+++ src/main/scala/Page.scala
@@ -0,0 +1,10 @@
+/**
+  * A web page with its url, pages and links
+  *
+  * @param url
+  * @param pages
+  * @param staticContent
+  */
+case class Page(url: String, pages: Set[String], staticContent: Set[String], externalUrls: Set[String]) {
+	override def toString = s"$url \nPages: $pages \nImages: $staticContent \nExternal links: $externalUrls"
+}
